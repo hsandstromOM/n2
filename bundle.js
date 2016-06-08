4bb870e4fffa0bd22ef9e58e642eaa770dd4a6d8
Michael Fausz <mcfausz@gmail.com>
2016-06-08 19:57:45 -0400

    HOME: Incorporated brand fonts into page layout. Created heading structure. CUSTOM-HOMES-DETAIL: Laid out Custom Homes Services section.

--- bundle.js
+++ bundle.js
@@ -198,28 +198,37 @@ angular
           number: 0,
           name: "Daniel Island Park",
           subtitle: "Residence - New Construction",
-          image: "http://fillmurray.com/600/350",
+          image: "http://placehold.it/600x350",
           heading: "A new construction for a new family",
           summary: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
           gallery: ["http://placehold.it/200x200","http://placehold.it/200x200","http://placehold.it/200x200","http://placehold.it/200x200","http://placehold.it/200x200","http://placehold.it/200x200","http://placehold.it/200x200","http://placehold.it/200x200",]
         },
         {
-          number: 1,
-          name: "Ralston Green",
-          subtitle: "Residence - New Construction",
-          image: "http://placehold.it/600x350",
-        },
-        {
-          number: 2,
-          name: "Ralston Green",
-          subtitle: "Residence - New Construction",
-          image: "http://placehold.it/600x350",
-        },
-        {
-          number: 3,
+          number: 0,
           name: "Daniel Island Park",
           subtitle: "Residence - New Construction",
-          image: "http://fillmurray.com/600/350",
+          image: "http://placehold.it/600x350",
+          heading: "A new construction for a new family",
+          summary: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+          gallery: ["http://placehold.it/200x200","http://placehold.it/200x200","http://placehold.it/200x200","http://placehold.it/200x200","http://placehold.it/200x200","http://placehold.it/200x200","http://placehold.it/200x200","http://placehold.it/200x200",]
+        },
+        {
+          number: 0,
+          name: "Daniel Island Park",
+          subtitle: "Residence - New Construction",
+          image: "http://placehold.it/600x350",
+          heading: "A new construction for a new family",
+          summary: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+          gallery: ["http://placehold.it/200x200","http://placehold.it/200x200","http://placehold.it/200x200","http://placehold.it/200x200","http://placehold.it/200x200","http://placehold.it/200x200","http://placehold.it/200x200","http://placehold.it/200x200",]
+        },
+        {
+          number: 0,
+          name: "Daniel Island Park",
+          subtitle: "Residence - New Construction",
+          image: "http://placehold.it/600x350",
+          heading: "A new construction for a new family",
+          summary: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+          gallery: ["http://placehold.it/200x200","http://placehold.it/200x200","http://placehold.it/200x200","http://placehold.it/200x200","http://placehold.it/200x200","http://placehold.it/200x200","http://placehold.it/200x200","http://placehold.it/200x200",]
         },
     ];
 
