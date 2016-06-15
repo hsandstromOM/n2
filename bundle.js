582b02564553196a74a4f9eb62628bdea9423775
Mike Fausz <mike@launchpeer.com>
2016-06-15 14:04:35 -0400

    CAREERS: Adjusted styles, processed data to fit design.

--- bundle.js
+++ bundle.js
@@ -216,12 +216,24 @@ angular
       window.careers = careerListings.data.items;
       vm.careerListings = careerListings.data.items;
 
+      var convertedToHTML;
+      var columnDivide;
+
       angular.forEach(vm.careerListings, function(careerListing) {
         console.log("career listing: " + careerListing.fields.careerTitle);
 
         console.log("raw: " + careerListing.fields.careerDescription);
         console.log("converted: " + markdown.toHTML(careerListing.fields.careerDescription));
-        careerListing.fields.careerDescriptionHTML = markdown.toHTML(careerListing.fields.careerDescription);
+        console.log("indexOf <h3>Resp: " + markdown.toHTML(careerListing.fields.careerDescription).indexOf('<h3>Responsibilities'));
+        convertedToHTML = markdown.toHTML(careerListing.fields.careerDescription);
+        columnDivide = convertedToHTML.indexOf('<h3>Responsibilities</h3>');
+        endIndex = convertedToHTML.indexOf('<h2>Are you our next team member?</h2>');
+        console.log("first col: " + convertedToHTML.slice(0, columnDivide));
+        console.log("second col: " + convertedToHTML.slice(columnDivide, endIndex));
+
+        // careerListing.fields.careerDescriptionHTML = markdown.toHTML(careerListing.fields.careerDescription);
+        careerListing.fields.careerDescriptionFirstCol = convertedToHTML.slice(0, columnDivide);
+        careerListing.fields.careerDescriptionSecondCol = convertedToHTML.slice(columnDivide, endIndex);
 
       });
     });
