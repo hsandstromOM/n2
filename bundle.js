4795dc5f2461ea86e021e0a6ad2a7ef7c7551183
Michael Fausz <mike@launchpeer.com>
2016-07-22 10:43:22 -0400

    CONTACT: Removed form input padding to display placeholders on Firefox.

--- bundle.js
+++ bundle.js
@@ -753,6 +753,7 @@ angular
 
     var currentState = 'HOME';
 
+    // CONTENTFUL
     const CONTENT_URL = 'https://cdn.contentful.com';
     const MEDIA_URL = 'https://images.contentful.com';
     const SPACE_ID = '80s1v057uxnv';
@@ -760,6 +761,9 @@ angular
 
     const GET_URL = CONTENT_URL + '/spaces/' + SPACE_ID + '/entries?access_token=' + API_KEY + '&content_type=';
 
+    // MAILCHIMP
+    const MAILCHIMP_KEY = '1029176b8a172367513eab75bfd1d6b0-us2';
+    
     function getPageTitle() {
       return pageTitle;
     }
@@ -987,12 +991,16 @@ angular
   NewsService.$inject = ['$http', '$q'];
 
   function NewsService($http, $q) {
+    // CONTENTFUL
     const CONTENT_URL = 'https://cdn.contentful.com';
     const MEDIA_URL = 'https://images.contentful.com';
     const SPACE_ID = '80s1v057uxnv';
     const API_KEY = '361c4996eb1e9c4236cea0b5c21701c76f302ec59f42c1b5111d365c7faee500';
 
     const GET_URL = CONTENT_URL + '/spaces/' + SPACE_ID + '/entries?access_token=' + API_KEY + '&content_type=';
+
+    // MAILCHIMP
+    const MAILCHIMP_KEY = '1029176b8a172367513eab75bfd1d6b0-us2';
 
     function getMainContent() {
       var defer = $q.defer();
