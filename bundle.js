55b8d0425e08cf0138f3f6804b1c4033b0239fa2
Michael Fausz <mike@launchpeer.com>
2016-07-22 18:55:46 -0400

    Pushing to test MailChimp integration.

--- bundle.js
+++ bundle.js
@@ -724,9 +724,15 @@ angular
   .controller('MainController', MainController);
 
 
-  function MainController(MainService, NewsService, $rootScope) {
+  function MainController(MainService, NewsService, $http) {
     var vm = this;
+    // const MAILCHIMP_USER = "nautilusco";
+    // const MAILCHIMP_DC = "us2";
+    // const MAILCHIMP_U = "91fe70fad31765c3ac6ebfa04";
+    // const MAILCHIMP_ID = "762b7f2fea";
+    const MAILCHIMP_KEY = '1029176b8a172367513eab75bfd1d6b0-us2';
 
+    const MAILCHIMP_URL = "https://us2.api.mailchimp.com/3.0/lists/762b7f2fea/members"
     vm.getPageTitle = function() {
       return MainService.getPageTitle();
     };
@@ -735,11 +741,26 @@ angular
       return MainService.getCurrentState();
     };
 
-    $rootScope.$on('$stateChangeSuccess', function (ev, to, toParams, from, fromParams) {
-      if (to.name == "news" && from.name !== "newsDetail") {
-        console.log('clear localStore');
-      }
-    });
+    vm.signUp = function() {
+      console.log("DOIN IT");
+      var req = {
+        "email_address": vm.signUpEmail,
+        "status": "pending",
+        // "merge_fields": {
+        //   "FNAME": "Urist",
+        //   "LNAME": "McVankab"
+      };
+
+      $http.post(MAILCHIMP_URL, req)
+        .then(
+          function(response){
+            console.log("success" + response);
+          },
+          function(response){
+            console.log("err" + response);
+          }
+        );
+    };
   }
 
 },{}],16:[function(require,module,exports){
