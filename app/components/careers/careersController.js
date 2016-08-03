f79430d9ce2d1e327d68f0aaa50c14e6d936fbc6
Michael Fausz <mike@launchpeer.com>
2016-08-02 21:59:00 -0400

    NEWS-DETAIL: Added Contentful field to embed video URL at end of News Post.

--- app/components/careers/careersController.js
+++ app/components/careers/careersController.js
@@ -38,7 +38,7 @@ angular
             career.fields.month = getMonthName(d.getMonth());
             career.fields.year = d.getFullYear();
             console.log(career);
-          })
+          });
         },
 
         // Error handler
@@ -47,22 +47,22 @@ angular
         }
       );
 
-      function getMonthName(monthNum) {
-        switch(monthNum) {
-          case 0: return 'January';
-          case 1: return 'February';
-          case 2: return 'March';
-          case 3: return 'April';
-          case 4: return 'May';
-          case 5: return 'June';
-          case 6: return 'July';
-          case 7: return 'August';
-          case 8: return 'September';
-          case 9: return 'October';
-          case 10: return 'November';
-          case 11: return 'December';
-        }
+    function getMonthName(monthNum) {
+      switch(monthNum) {
+        case 0: return 'January';
+        case 1: return 'February';
+        case 2: return 'March';
+        case 3: return 'April';
+        case 4: return 'May';
+        case 5: return 'June';
+        case 6: return 'July';
+        case 7: return 'August';
+        case 8: return 'September';
+        case 9: return 'October';
+        case 10: return 'November';
+        case 11: return 'December';
       }
+    }
 
     vm.toggleListing = function(careerId) {
       vm.currentListing = vm.currentListing === careerId ? '' : careerId;
